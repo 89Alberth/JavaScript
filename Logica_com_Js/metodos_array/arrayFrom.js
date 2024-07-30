d8b24906af75197aea44e1b1b6fbff2ab3326a02
Alberth R C santana <alberthcosta89@gmail.com>
2024-07-30 19:41:47 -0300

    Estudos

---
+++ Logica_com_Js/metodos_array/arrayFrom.js
@@ -0,0 +1,6 @@
+const nome = "Alberth"
+console.log(Array.from(nome))
+
+const arr = Array.of("Alberth", "Samira", "Arthur", "Franciele")
+console.log(arr[2])
+
---
+++ Logica_com_Js/metodos_array/destructuring.js
@@ -0,0 +1,5 @@
+const numeros = [ 89, 95, 30]
+
+const [n1, ,n2] = numeros
+
+console.log(n1, n2)
