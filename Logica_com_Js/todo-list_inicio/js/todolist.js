aba38a17b1a88578719ffade4706af322f763c0f
Alberth R C santana <alberthcosta89@gmail.com>
2024-09-04 20:45:49 -0300

    Estudos

--- Logica_com_Js/todo-list_inicio/js/todolist.js
+++ Logica_com_Js/todo-list_inicio/js/todolist.js
@@ -4,39 +4,63 @@
     const itemInput = document.getElementById("item-input") //campo input 
     const todoAddForma = document.getElementById("todo-add") //formulario
     const ul = document.getElementById("todo-list")
-    const lis = ul.getElementsByTagName("li")
+    // const lis = ul.getElementsByTagName("li")
 
-    function addTask(task) {
-        const li = document.createElement("li")
-        li.className = "todo-item"
-        const p = document.createElement("p")
-        p.className = "task-name"
-        p.textContent = task
-        li.appendChild(p)
-        ul.appendChild(li)
+   
+    let arrTask = []
+        
 
-        li.addEventListener("click", function(){
+    function addEventLi(li) {
+        li.addEventListener("click", function () {
             console.log(this)
         })
     }
 
+
+
+    function generateTaskLi(obj) {
+        const li = document.createElement("li")
+        li.className = "todo-item"
+        const p = document.createElement("p")
+        p.className = "task-name"
+        p.textContent = obj.name
+        li.appendChild(p)
+        addEventLi(li)
+
+        return li
+    }
+
+
+
+    function renderTask() { 
+        ul.innerHTML = ""
+        arrTask.forEach(task => {
+            ul.appendChild(generateTaskLi(task))
+        })
+    }
+
+
+
+
+    function addTask(task) {
+        arrTask.push({
+            name: task,
+            creatAt: Date.now(),
+            completed: false
+        })
+    }
+
+
+
+
     todoAddForma.addEventListener("submit", function (e) {
         e.preventDefault()
-        // ul.innerHTML += `
-        // <li class="todo-item">
-        //     <p class="task-name">${itemInput.value}</p>
-        // </li>`
         addTask(itemInput.value)
+        renderTask()
         itemInput.value = ""
         itemInput.focus()
-
     });
 
-    [...lis].forEach(element => {
-        element.addEventListener("click", function(){
-            console.log(this)
-        }) 
-    })
-
+    renderTask()
 
 })();
