ff3e2b3ded85dbd7c60c6945b387e184c2eee09b
Alberth R C santana <alberthcosta89@gmail.com>
2024-09-26 18:24:42 -0300

    estudos

--- Logica_com_Js/todo-list_inicio/js/todolist.js
+++ Logica_com_Js/todo-list_inicio/js/todolist.js
@@ -6,15 +6,28 @@
     const ul = document.getElementById("todo-list")
     const lis = ul.getElementsByTagName("li")
 
-   
-    let arrTask = [
-        {
-            name: "task 1",
-            creatAt: Date.now(),
-            completed: false
-        }
-    ]
-        
+
+    let arrTask = getSaveData()
+
+    function getSaveData(){
+        let taskData =  localStorage.getItem("tasks")
+        taskData = JSON.parse(taskData)
+        return taskData && taskData.length ? taskData : [
+            {
+                name: "task 1",
+                creatAt: Date.now(),
+                completed: false
+            }
+        ]
+    
+    
+    }
+
+    function setnewData(){
+        localStorage.setItem("tasks", JSON.stringify(arrTask))
+    }
+
+    setnewData()
 
     // function addEventLi(li) {
     //     li.addEventListener("click", function () {
@@ -34,7 +47,7 @@
         li.className = "todo-item"
 
         checkButton.className = "button-check"
-        checkButton.innerHTML = '<i class="fas fa-check displayNone"></i>'
+        checkButton.innerHTML = `<i class="fas fa-check ${obj.completed ? "" : "displayNone"}" data-action="checkButton"></i>`
         checkButton.setAttribute("data-action", "checkButton")
         li.appendChild(checkButton)
 
@@ -48,15 +61,19 @@
 
         const containerEdit = document.createElement("div")
         containerEdit.className = "editContainer"
+
         const inputEdit = document.createElement("input")
         inputEdit.className = "editInput"
         inputEdit.setAttribute("type", "text")
+        inputEdit.value = obj.name
         containerEdit.appendChild(inputEdit)
+
         const containerEditButton = document.createElement("button")
         containerEditButton.className = "editButton"
         containerEditButton.textContent = "Edit"
         containerEditButton.setAttribute("data-action", "containerEditButton")
         containerEdit.appendChild(containerEditButton)
+
         const containerCancelButton = document.createElement("button")
         containerCancelButton.className = "cancelButton"
         containerCancelButton.textContent = "Cancel"
@@ -71,13 +88,13 @@
         removeTaskButton.setAttribute("data-action", "deleteButton")
         li.appendChild(removeTaskButton)
 
-       
+
         return li
     }
 
 
 
-    function renderTask() { 
+    function renderTask() {
         ul.innerHTML = ""
         arrTask.forEach(task => {
             ul.appendChild(generateTaskLi(task))
@@ -93,33 +110,68 @@
             creatAt: Date.now(),
             completed: false
         })
+        setnewData()
     }
 
 
 
-    function clickedUl(e){
+    function clickedUl(e) {
         const dataAction = e.target.getAttribute("data-action")
         console.log(dataAction)
-        if(!dataAction) return
+        if (!dataAction) return
 
         let currentLi = e.target
         console.log(currentLi)
 
-        while(currentLi.nodeName !== "LI"){
+        while (currentLi.nodeName !== "LI") {
             currentLi = currentLi.parentElement
         }
-        
+
         const currentLiIndex = [...lis].indexOf(currentLi)
 
         const actions = {
-            deleteButton: function(){
+            deleteButton: function () {
                 arrTask.splice(currentLiIndex, 1)
                 renderTask()
+                setnewData()
                 console.log(arrTask)
+            },
+
+            editButton: function () {
+                const editContainer = currentLi.querySelector(".editContainer");
+                [...ul.querySelectorAll(".editContainer")].forEach(container => {
+                    container.removeAttribute("style")
+                })
+                editContainer.style.display = "flex"
+            },
+
+            containerEditButton: function () {
+                const val = document.querySelector(".editInput").value
+                arrTask[currentLiIndex].name = val
+                renderTask()
+                setnewData()
+            },
+
+            containerCancelButton: function () {
+                currentLi.querySelector(".editContainer").removeAttribute("style")
+                currentLi.querySelector(".editInput").value = arrTask[currentLiIndex].name
+            },
+
+            checkButton: function () {
+                arrTask[currentLiIndex].completed = !arrTask[currentLiIndex].completed
+
+                // if(arrTask[currentLiIndex].completed){
+                //     currentLi.querySelector(".fa-check").classList.remove("displayNone")
+                // }else{
+                //     currentLi.querySelector(".fa-check").classList.add("displayNone")
+                // }
+
+                renderTask()
+                setnewData()
             }
         }
 
-        if(actions[dataAction]){
+        if (actions[dataAction]) {
             actions[dataAction]()
         }
     }
