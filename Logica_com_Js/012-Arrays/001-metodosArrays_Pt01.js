c82bb66321300b531ba5a16eecfdfe36b804ac5c
Alberth R C santana <alberthcosta89@gmail.com>
2025-01-01 12:43:21 -0300

    estudos metodos

--- Logica_com_Js/012-Arrays/001-metodosArrays_Pt01.js
+++ Logica_com_Js/012-Arrays/001-metodosArrays_Pt01.js
@@ -10,6 +10,8 @@ console.log("Every: ", onlyNumber)
 
 
 
+
+
 //some retorna um boolean true se ao menos um elemento do array atenda requisito da função passada, se todos os elementos não atender o requisito retorna false
 
 const onlyNumber2 = mainArray.some(function (el) {
@@ -19,17 +21,39 @@ const onlyNumber2 = mainArray.some(function (el) {
 console.log("Some: ", onlyNumber2)
 
 
+
+
 //forEach sempre retonara undefined,Ele execulta uma função para cada elemento do array 
 
 const arrForeach = [5, 8, 17, 4, 6]
 let sum = 0
 
 arrForeach.forEach(function (el) {
-    sum += el 
+    sum += el
 })
 
 console.log("forEach: ", sum)
 
 
 
+//map executa uma função para cada item do array, e retorna um novo array com os novos resultados
+const arraymap = [1, 100, 8, 94]
+
+const callbackMap = function (el) {
+    return el * 2
+}
+console.log("Map: ", arraymap.map(callbackMap))
+
+
+
+//filter Ele retorna um novo array com todos elementos que passaram no teste  aplicado
+
+const idadePessoas = [16, 32, 64, 12, 25, 89, 45, 5]
+
+const idadeFiltrada = idadePessoas.filter(function (el){
+    return el >= 18
+})
+
+console.log("Array original: ", idadePessoas)
+console.log("Array filtrado: ", idadeFiltrada)
 
---
+++ Logica_com_Js/012-Arrays/002-metodosArray_Pt02.js
@@ -0,0 +1,8 @@
+const numbers = [2, 10, 5, 89, 23, 35, 89]
+
+//Qualquer um dos dois métodos (indexOf ou LastIndexOf) retorna -1 se caso não encontrar nada
+//indexOf retorna o indice do primeiro elemento encontrado  que foi pesquisado dentro de um array
+console.log("IndexOf: ", numbers.indexOf(89))
+
+//LastindexOf retorna o indice do ultimo elemento encontrado  que foi pesquisado dentro de um array
+console.log("LastIndexOf: ", numbers.lastIndexOf(89))
