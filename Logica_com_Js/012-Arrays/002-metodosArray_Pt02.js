4f9bf56911580a6ca68c67b0f65c75f50c95c87c
Alberth R C santana <alberthcosta89@gmail.com>
2025-01-01 12:44:11 -0300

    estudos

--- Logica_com_Js/012-Arrays/002-metodosArray_Pt02.js
+++ Logica_com_Js/012-Arrays/002-metodosArray_Pt02.js
@@ -1,8 +1,8 @@
 const numbers = [2, 10, 5, 89, 23, 35, 89]
 
-//Qualquer um dos dois métodos (indexOf ou LastIndexOf) retorna -1 se caso não encontrar nada
+//Qualquer um dos dois métodos (indexOf ou LastIndexOf) retorna -1 se caso não encontrar o elemento que foi pedido
 //indexOf retorna o indice do primeiro elemento encontrado  que foi pesquisado dentro de um array
-console.log("IndexOf: ", numbers.indexOf(89))
+console.log("IndexOf: ", numbers.indexOf(88))
 
 //LastindexOf retorna o indice do ultimo elemento encontrado  que foi pesquisado dentro de um array
-console.log("LastIndexOf: ", numbers.lastIndexOf(89))
+console.log("LastIndexOf: ", numbers.lastIndexOf(87))
